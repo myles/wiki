2bcff1427af4c7cc59eae956cac022f47462c384
Myles Braithwaite <me@mylesbraithwaite.com>
2019-06-01 18:31:59 -0400

    :heavy_plus_sign: Add a search and feed plugin.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -12,7 +12,15 @@ module.exports = {
           message: "New content is available.",
           buttonText: "Refresh"
         }
-      }
+      },
+      "vuepress-plugin-feed": {
+        title: "Myles' Wiki",
+        description: "A place Myles Braithwaite can try and share everything he researches and knows about a variety of topics.",
+        canonical_base: "https://wiki.mylesb.ca",
+      },
+      "@vuepress/search": {
+        searchMaxSuggestions: 10
+      },
     },
     sidebar: {
       "/setup/": [

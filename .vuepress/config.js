874d2d98a5feb6aaf988a5c828ab57fe5b638f0e
Myles Braithwaite <me@mylesbraithwaite.com>
2020-04-04 16:34:12 -0400

    :construction: Did some work and don't know how to categorise it.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,21 +6,23 @@ module.exports = {
     repo: 'myles/wiki',
     editLinks: true,
     lastUpdated: 'Last Updated',
-    algolia: {
-      apiKey: process.env.ALGOLIA_API_KEY || '',
-      indexName: process.env.ALGOLIA_INDEX_NAME || 'documents',
-    },
     plugins: {
-      'vuepress-plugin-feed': {
-        title: "Myles' Wiki",
-        description: 'A place Myles Braithwaite can try and share everything he researches and knows about a variety of topics.',
-        canonical_base: 'https://wiki.mylesb.ca',
-      },
       '@vuepress/search': {
         searchMaxSuggestions: 10,
       },
     },
     sidebar,
+  },
+  plugins: {
+    'vuepress-plugin-feed': {
+      enable: true,
+      title: "Myles' Wiki",
+      description: 'A place Myles Braithwaite can try and share everything he researches and knows about a variety of topics.',
+      canonical_base: 'https://wiki.mylesb.ca',
+    },
+    'vuepress-plugin-sitemap': {
+      hostname: 'https://wiki.mylesb.ca',
+    },
   },
   markdown: {
     lineNumbers: true,
